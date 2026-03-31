e809eb04c320d8ba6d69c5fe0d29e5900c0a1bf2
Aditya-amrahs <122723977+Aditya-amrahs@users.noreply.github.com>
2026-04-01 02:57:18 +0530

    Database, schemas, auth, CRUD (#2)

--- app/database.py
+++ app/database.py
@@ -4,19 +4,14 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./realestate.db"
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
+# for routes
 def get_db():
     db = SessionLocal()
     try:
